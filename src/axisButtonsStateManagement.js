9ac4268574a94b75c17aac4e2747e72d1585c5cd
late-bloomer82 <nateriver72@outlook.com>
2024-06-27 11:25:18 -0400

    feat: axis buttons state management

---
+++ src/axisButtonsStateManagement.js
@@ -0,0 +1,29 @@
+let state = true;
+
+export function toggleButtonState() {
+  const xButton = document.getElementById("xAxisButton");
+  const yButton = document.getElementById("yAxisButton");
+
+  xButton.addEventListener("click", () => {
+    xButton.classList.remove("inactive");
+    xButton.classList.add("active");
+    yButton.classList.add("inactive");
+    state = true;
+  });
+  yButton.addEventListener("click", () => {
+    yButton.classList.remove("inactive");
+    yButton.classList.add("active");
+    xButton.classList.add("inactive");
+    state = false;
+  });
+}
+
+export function checkButtonState() {
+  //if x axis
+  if (state) {
+    return true;
+  } //if y axis
+  else {
+    return false;
+  }
+}
