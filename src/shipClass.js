c21af86c58dc166148404675c2efdc612e215a35
late-bloomer82 <nateriver72@outlook.com>
2024-07-17 16:50:04 -0400

    added a position property and initialized the player's and computer's ship

--- src/shipClass.js
+++ src/shipClass.js
@@ -1,8 +1,10 @@
 export class Ship {
-  constructor(length, hitCount = 0, status = "floating") {
+  constructor(length, hitCount = 0, status = "floating", name, position) {
+    this.name = name;
     this.length = length;
     this.hitCount = hitCount;
     this.status = status;
+    this.position = position;
   }
 
   hit() {
@@ -17,3 +19,15 @@ export class Ship {
     }
   }
 }
+
+export const playerCarrier = new Ship(5, 0, "floating", "carrier");
+export const playerBattleship = new Ship(4, 0, "floating", "battleship");
+export const playerCruiser = new Ship(4, 0, "floating", "cruiser");
+export const playerSubmarine = new Ship(3, 0, "floating", "submarine");
+export const playerDestroyer = new Ship(2, 0, "floating", "destroyer");
+
+export const computerCarrier = new Ship(5, 0, "floating", "carrier");
+export const computerBattleship = new Ship(4, 0, "floating", "battleship");
+export const computerCruiser = new Ship(4, 0, "floating", "cruiser");
+export const computerSubmarine = new Ship(3, 0, "floating", "submarine");
+export const computerDestroyer = new Ship(2, 0, "floating", "destroyer");
