65bc071c8b1517d445a348f9b30937c6a1e95a19
late-bloomer82 <nateriver72@outlook.com>
2024-07-25 13:20:00 -0400

    basic game page layout and structure

---
+++ src/domGamePage.js
@@ -0,0 +1,73 @@
+import { createGameboardContainer } from "./domSetupPage";
+import enemyImg from "./images/enemy.png";
+import { allySrc } from "./domSetupPage";
+
+export function createGamePage() {
+  const gamePageContainer = document.createElement("div");
+  gamePageContainer.id = "game-page";
+  const gameContainer = document.createElement("div");
+  gameContainer.id = "game-container";
+  document.body.appendChild(gamePageContainer);
+  gamePageContainer.appendChild(gameContainer);
+  const gameboardArea = createGameboardArea();
+  const quoteArea = createQuoteArea();
+  gameContainer.append(gameboardArea, quoteArea);
+  createGrid("player-grid");
+  createGrid("computer-grid");
+}
+
+function createGameboardArea() {
+  const gameboardSection = document.createElement("section");
+  gameboardSection.id = "gameboardSection";
+  createGameboardContainer(gameboardSection, "player-grid", "player-gameboard");
+  createGameboardContainer(
+    gameboardSection,
+    "computer-grid",
+    "computer-gameboard"
+  );
+
+  return gameboardSection;
+}
+
+function createQuoteArea() {
+  const quoteSection = document.createElement("section");
+  quoteSection.id = "quoteSection";
+  createQuoteBoxes(
+    quoteSection,
+    allySrc,
+    "Our formation is set and ready for action captain. We are awaiting your orders."
+  );
+  createQuoteBoxes(
+    quoteSection,
+    enemyImg,
+    "You will regret the day you started this. I will end you."
+  );
+  return quoteSection;
+}
+
+function createQuoteBoxes(parent, img, string) {
+  const messageBox = document.createElement("section");
+  messageBox.className = "message-container";
+  parent.appendChild(messageBox);
+
+  const avatar = document.createElement("img");
+  avatar.src = img;
+  avatar.className = "avatar";
+  avatar.style.height = "80%";
+  messageBox.appendChild(avatar);
+
+  const quoteParagraph = document.createElement("p");
+  quoteParagraph.textContent = string;
+  messageBox.appendChild(quoteParagraph);
+}
+
+function createGrid(grid) {
+  const gameboardGrid = document.getElementById(grid);
+  console.log(gameboardGrid);
+
+  for (let i = 0; i < 100; i++) {
+    const square = document.createElement("div");
+    square.className = "squares";
+    gameboardGrid.appendChild(square);
+  }
+}
