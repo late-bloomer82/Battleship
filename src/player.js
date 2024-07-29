aff7efeecc6e60b2409c0185aa80cad26d202afe
late-bloomer82 <nateriver72@outlook.com>
2024-07-29 15:37:51 -0400

    created player and computer instances and associated their respective gameboards

--- src/player.js
+++ src/player.js
@@ -1,6 +1,11 @@
+import { computerGameboard, playerGameboard } from "./gameboard";
+
 class Player {
   constructor(type, gameboard) {
     this.type = type;
     this.gameboard = gameboard;
   }
 }
+
+export const player = new Player("real", playerGameboard);
+export const computer = new Player("computer", computerGameboard);
