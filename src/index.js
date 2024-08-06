f4a2e510f78c85e6a96aa85305626094254e565d
late-bloomer82 <nateriver72@outlook.com>
2024-08-06 19:28:44 -0400

    Human game turn basic functionality implemented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
-import { Gameboard } from "./gameboard";
-import { Ship } from "./shipClass";
-import { Player } from "./player";
-import { createSetupPage, enterCombatBtn } from "./domSetupPage";
-import { dragNdrop } from "./dragNdrop";
+import { computerGameboard } from "./classes/gameboard";
+import { computer } from "./classes/player";
+import { setupComputerGameboard } from "./computerFunctionality";
+import {
+  handleHitSquare,
+  handleMissedSquare,
+  revealShipIfSunk,
+} from "./dom/domGameActions";
+import { createSetupPage, enterCombatBtn } from "./dom/domSetupPage";
+import { getMousePercentageCoordinates } from "./dragNDropFunctionality";
 
 enterCombatBtn.addEventListener("click", createSetupPage);
+console.log("hi", computerGameboard);
+console.log(computer.ships);
+setupComputerGameboard();
+
+//Player turn state management
+const isHumanTurn = true;
+
+//Drive the game using event listeners
+
+//Human turn
+if (isHumanTurn) {
+  document.body.addEventListener("click", (event) => {
+    if (event.target.matches("#computer-grid .squares")) {
+      const clickedSquare = event.target;
+      const computerGrid = document.getElementById("computer-grid");
+      const { xPercent, yPercent } = getMousePercentageCoordinates(
+        event,
+        computerGrid
+      );
+      const x = computerGameboard.percentageToGridCoordinate(xPercent);
+      const y = computerGameboard.percentageToGridCoordinate(yPercent);
+      const isShipHit = computerGameboard.receiveAttack([x, y]);
+
+      if (isShipHit) {
+        handleHitSquare(clickedSquare);
+        revealShipIfSunk(computerGrid);
+      } else {
+        handleMissedSquare(clickedSquare);
+      }
+    }
+  });
+} else if (!isHumanTurn) {
+  //
+}
+//
