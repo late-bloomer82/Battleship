37fc3d8053c8f78fa0ee2e7dc298cf1fb6cd9a0c
late-bloomer82 <nateriver72@outlook.com>
2024-08-06 19:30:08 -0400

    dom actions/functions for game turn

---
+++ src/dom/domGameActions.js
@@ -0,0 +1,60 @@
+import {
+  computerBattleship,
+  computerCarrier,
+  computerCruiser,
+  computerDestroyer,
+  computerSubmarine,
+} from "../classes/ship";
+import { setShip } from "./renderGameboard";
+import {
+  carrierSrc,
+  battleshipSrc,
+  cruiserSrc,
+  submarineSrc,
+  destroyerSrc,
+} from "./domSetupPage";
+
+export function handleHitSquare(square) {
+  const dot = document.createElement("div");
+  square.appendChild(dot);
+  square.classList.add("hit-disabled");
+}
+
+export function handleMissedSquare(square) {
+  const dot = document.createElement("div");
+  square.appendChild(dot);
+  square.classList.add("missed-disabled");
+}
+
+const computerShips = [
+  { instance: computerCarrier, src: carrierSrc, width: "50%" },
+  { instance: computerBattleship, src: battleshipSrc, width: "40%" },
+  { instance: computerCruiser, src: cruiserSrc, width: "30%" },
+  { instance: computerSubmarine, src: submarineSrc, width: "30%" },
+  { instance: computerDestroyer, src: destroyerSrc, width: "20%" },
+];
+
+export function revealShipIfSunk(computerGrid) {
+  let indexToRemove = null;
+  for (let i = 0; i < computerShips.length; i++) {
+    if (computerShips[i].instance.isSunk()) {
+      setShip(
+        {
+          src: computerShips[i].src,
+          width: computerShips[i].width,
+          shipInstance: computerShips[i].instance,
+          position: computerShips[i].instance.position,
+        },
+        computerGrid
+      );
+      indexToRemove = i;
+    }
+  }
+  if (indexToRemove != null) {
+    computerShips.splice(indexToRemove, 1);
+  }
+}
+
+// function humanWon() {}
+
+// function computerWon() {}
