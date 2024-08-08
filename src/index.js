d741bf116e468dd7aa6c06f9001dc29cbd6be6f0
late-bloomer82 <nateriver72@outlook.com>
2024-08-08 16:56:13 -0400

    added computer turn logic and refactored the code inside functions to remove event listener during computer turn. also removed now-redundant state variable

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import { computerGameboard } from "./classes/gameboard";
+import { computerGameboard, playerGameboard } from "./classes/gameboard";
 import { computer } from "./classes/player";
-import { setupComputerGameboard } from "./computerFunctionality";
+import {
+  findAttackedSquare,
+  randomAttackGenerator,
+  setupComputerGameboard,
+} from "./computerFunctionality";
 import {
   handleHitSquare,
   handleMissedSquare,
   revealShipIfSunk,
+  selectComputerGrid,
+  selectPlayerSquares,
 } from "./dom/domGameActions";
 import { createSetupPage, enterCombatBtn } from "./dom/domSetupPage";
 import { getMousePercentageCoordinates } from "./dragNDropFunctionality";
@@ -14,34 +20,48 @@ console.log("hi", computerGameboard);
 console.log(computer.ships);
 setupComputerGameboard();
 
-//Player turn state management
-const isHumanTurn = true;
+// Start the game by enabling the human player's turn
+handleHumanTurn();
 
-//Drive the game using event listeners
-
-//Human turn
-if (isHumanTurn) {
-  document.body.addEventListener("click", (event) => {
-    if (event.target.matches("#computer-grid .squares")) {
-      const clickedSquare = event.target;
-      const computerGrid = document.getElementById("computer-grid");
-      const { xPercent, yPercent } = getMousePercentageCoordinates(
-        event,
-        computerGrid
-      );
-      const x = computerGameboard.percentageToGridCoordinate(xPercent);
-      const y = computerGameboard.percentageToGridCoordinate(yPercent);
-      const isShipHit = computerGameboard.receiveAttack([x, y]);
-
-      if (isShipHit) {
-        handleHitSquare(clickedSquare);
-        revealShipIfSunk(computerGrid);
-      } else {
-        handleMissedSquare(clickedSquare);
-      }
-    }
-  });
-} else if (!isHumanTurn) {
-  //
+function handleHumanTurn() {
+  document.body.addEventListener("click", onHumanClick);
 }
-//
+
+function handleComputerTurn() {
+  // Remove event listener to disable human interaction
+  document.body.removeEventListener("click", onHumanClick);
+  // Computer's attack logic
+  const [x, y] = randomAttackGenerator();
+  const isShipHit = playerGameboard.receiveAttack([x, y]);
+  const playerSquares = selectPlayerSquares();
+  const attackedSquare = findAttackedSquare(x, y, playerSquares);
+
+  if (isShipHit) {
+    handleHitSquare(attackedSquare);
+  } else {
+    handleMissedSquare(attackedSquare);
+    handleHumanTurn(); // Re-enable human turn
+  }
+}
+
+function onHumanClick(event) {
+  if (event.target.matches("#computer-grid .squares")) {
+    const clickedSquare = event.target;
+    const computerGrid = selectComputerGrid();
+    const { xPercent, yPercent } = getMousePercentageCoordinates(
+      event,
+      computerGrid
+    );
+    const x = computerGameboard.percentageToGridCoordinate(xPercent);
+    const y = computerGameboard.percentageToGridCoordinate(yPercent);
+    const isShipHit = computerGameboard.receiveAttack([x, y]);
+
+    if (isShipHit) {
+      handleHitSquare(clickedSquare);
+      revealShipIfSunk(computerGrid);
+    } else {
+      handleMissedSquare(clickedSquare);
+      handleComputerTurn();
+    }
+  }
+}
