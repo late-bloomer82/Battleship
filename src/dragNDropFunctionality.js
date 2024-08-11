c364d06918782eb3499725e28d29874d72f27f7f
late-bloomer82 <nateriver72@outlook.com>
2024-08-11 15:51:38 -0400

    big commit sorry. added functionality that highlights grid squares relative to the size of the ship that is being dragged over the grid. Also checks for taken unavailable(taken) squares or out of boundspositioning and highlights squares accordingly. fixed issue of ship axis property being modified improperly. Added functionality that enables confirm button only when all ships have been placed and functions that reset data structures when reset button gets clicked.

--- src/dragNDropFunctionality.js
+++ src/dragNDropFunctionality.js
@@ -5,7 +5,7 @@ import {
 } from "./dom/shipPlacement";
 import { checkButtonState } from "./axisButtonsStateManagement";
 import { playerGameboard } from "./classes/gameboard";
-import { getDraggedShipDetails } from "./dom/domSetupPage";
+import { getDraggedShipDetails, updateConfirmButton } from "./dom/domSetupPage";
 import {
   playerBattleship,
   playerCarrier,
@@ -13,6 +13,11 @@ import {
   playerDestroyer,
   playerSubmarine,
 } from "./classes/ship";
+import { humanUser } from "./classes/player";
+// I tried using event.dataTransfer.setData and event.dataTransfer.getData to retrieve the ship that is being dragged's info
+// but it simply doesnt work because of browser security measures that make the data in dataTransfer only available on drop.
+// Had no choice but to use a global variable despite it being frowned upon :/
+let draggedShipId = null;
 
 export function dragNdrop() {
   const squares = document.querySelectorAll(".squares");
@@ -28,27 +33,107 @@ export function dragNdrop() {
   gridContainer.addEventListener("drop", handleDrop);
 
   squares.forEach((square) => {
-    square.addEventListener("dragenter", handleDragEnter);
     square.addEventListener("dragleave", handleDragLeave);
   });
 }
 
-function handleDragStart(event) {
-  event.dataTransfer.setData("text/plain", event.target.id);
-}
-
 function handleDragOver(event) {
   event.preventDefault();
+  const setupGrid = document.getElementById("setup-grid");
+  const playerSquares = setupGrid.querySelectorAll(".squares");
+  const draggedShip = getShipObject(draggedShipId);
+  const { xPercent, yPercent } = getMousePercentageCoordinates(
+    event,
+    setupGrid
+  );
+  const x = playerGameboard.percentageToGridCoordinate(xPercent);
+  const y = playerGameboard.percentageToGridCoordinate(yPercent);
+  const hoveredSquares = findHoveredSquares(
+    x,
+    y,
+    playerSquares,
+    draggedShip.length
+  );
+  if (isHoveredAreaUnavailable(x, y, draggedShip.length)) {
+    hoveredSquares.forEach((hoveredSquare) => {
+      if (hoveredSquare != undefined) {
+        hoveredSquare.classList.add("highlight");
+        hoveredSquare.classList.add("red");
+      }
+    });
+  } else if (!isHoveredAreaUnavailable(x, y, draggedShip.length)) {
+    hoveredSquares.forEach((hoveredSquare) => {
+      if (hoveredSquare != undefined) {
+        hoveredSquare.classList.add("highlight");
+        hoveredSquare.classList.add("blue");
+      }
+    });
+  }
 }
 
-function handleDragEnter(event) {
-  event.preventDefault();
-  event.target.classList.add("highlight");
+function findHoveredSquares(x, y, squares, length) {
+  const squaresArrayIndexes = [];
+  const hoveredSquares = [];
+  for (let i = 0; i < length; i++) {
+    const hoveredSquareIndex = playerGameboard.gameboard.findIndex(
+      (coordinate) =>
+        coordinate.coordinates[0] === y && coordinate.coordinates[1] === x
+    ); /*The reason im inverting the x and y is because i need to find the opposite coordinate(x and y inversed) since the 
+     gameboard array coordinates are initalized in a vertical order while the DOM squares nodes array is returned or initalized horizontally. 
+     So to get the accurate index for the DOM squares array,inverting the x and y in the findIndex function is needed.*/
+    squaresArrayIndexes.push(hoveredSquareIndex);
+    if (checkButtonState()) {
+      x++;
+    } else {
+      y++;
+    }
+  }
+  squaresArrayIndexes.forEach((arrayIndex) => {
+    hoveredSquares.push(squares[arrayIndex]);
+  });
+  return hoveredSquares;
 }
 
-function handleDragLeave(event) {
-  event.preventDefault();
-  event.target.classList.remove("highlight");
+function isHoveredAreaUnavailable(x, y, length) {
+  //gameboard object coordinate
+  for (let i = 0; i < length; i++) {
+    const hoveredCoordinate = playerGameboard.gameboard.find(
+      (coordinate) =>
+        coordinate.coordinates[0] === x && coordinate.coordinates[1] === y
+    );
+    if (hoveredCoordinate === undefined) {
+      return true;
+    }
+    if (hoveredCoordinate.ship != null) {
+      return true;
+    }
+
+    if (checkButtonState()) {
+      x++;
+    } else {
+      y++;
+    }
+  }
+  return false;
+}
+
+function handleDragStart(event) {
+  draggedShipId = event.target.id;
+  event.dataTransfer.setData("text/plain", draggedShipId);
+}
+
+function handleDragLeave() {
+  const setupGrid = document.getElementById("setup-grid");
+  const highlightedSquares = setupGrid.querySelectorAll(".squares.highlight");
+
+  highlightedSquares.forEach((playerSquare) => {
+    if (
+      playerSquare.classList.contains("blue") ||
+      playerSquare.classList.contains("red")
+    ) {
+      playerSquare.classList.remove("highlight", "blue", "red");
+    }
+  });
 }
 
 // Function to calculate the position relative to the grid container in percentages
@@ -103,12 +188,20 @@ function getShipObject(containerId) {
 function handleDrop(event) {
   event.preventDefault();
 
-  const square = event.target;
   const gridContainer = document.querySelector(".gameboard-grid");
+  const highlightedSquares =
+    gridContainer.querySelectorAll(".squares.highlight");
   const { draggedShipId, draggedContainer, draggedShipImg } =
     getDraggedShipDetails(event);
 
-  square.classList.remove("highlight");
+  highlightedSquares.forEach((playerSquare) => {
+    if (
+      playerSquare.classList.contains("blue") ||
+      playerSquare.classList.contains("red")
+    ) {
+      playerSquare.classList.remove("highlight", "blue", "red");
+    }
+  });
   const shipObject = getShipObject(draggedShipId);
   const { xPercent, yPercent } = getMousePercentageCoordinates(
     event,
@@ -131,24 +224,61 @@ function handleDrop(event) {
   //If y button selected
   if (!checkButtonState()) {
     modifyShipAxisProperty(draggedShipId);
-    rotateImage(draggedShipImg, 90);
   }
-
-  placeShip(
-    draggedShipId,
-    draggedShipImg,
-    draggedContainer,
-    snappedXPercent,
-    snappedYPercent,
-    gridContainer
-  );
-
-  playerGameboard.placeShipObject(
-    snappedXPercent,
-    snappedYPercent,
-    shipObject,
-    shipObject.length
-  );
-
+  if (
+    !playerGameboard.placeShipObject(
+      snappedXPercent,
+      snappedYPercent,
+      shipObject,
+      shipObject.length
+    )
+  ) {
+    //If y button selected
+    if (!checkButtonState()) {
+      rotateImage(draggedShipImg, 90);
+    }
+    console.log("in");
+    placeShip(
+      draggedShipId,
+      draggedShipImg,
+      draggedContainer,
+      snappedXPercent,
+      snappedYPercent,
+      gridContainer
+    );
+  }
+  if (enableConfirmButton()) {
+    updateConfirmButton();
+  }
   console.log(playerGameboard);
+}
+
+function enableConfirmButton() {
+  const humanShips = humanUser.ships;
+  //Check if theres at least one ship that is not placed
+  for (const ship in humanShips) {
+    const shipObject = humanShips[ship];
+    if (shipObject.position === undefined) {
+      return false;
+    }
+  }
+  //If all ship placed ,return true
+  return true;
+}
+
+export function resetGameboard() {
+  playerGameboard.gameboard.forEach((coordinate) => {
+    if (coordinate.ship != null) {
+      coordinate.ship = null;
+    }
+  });
+}
+
+export function resetPlayerShips() {
+  const humanShips = humanUser.ships;
+
+  for (const ship in humanShips) {
+    const shipObject = humanShips[ship];
+    shipObject.position = undefined;
+  }
 }
