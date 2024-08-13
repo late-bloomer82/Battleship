a849d18bad744041d5f4b4bd1cd92878ba7f5ba4
late-bloomer82 <nateriver72@outlook.com>
2024-08-13 18:33:50 -0400

    modified createQuoteBoxes function to implement typing animation and start game upon end of animation. Also added a few parameters for ally and enemy message box customization.

--- src/dom/domGamePage.js
+++ src/dom/domGamePage.js
@@ -3,6 +3,8 @@ import enemySrc from "../images/enemy.png";
 import { allySrc } from "./domSetupPage";
 import { populatePlayerGameboard } from "./renderGameboard";
 import { computerGameboard } from "../classes/gameboard";
+import { typeMessage } from "../messageFunctionality";
+import { humanTurn } from "..";
 export { enemySrc };
 export function createGamePage() {
   const gamePageContainer = document.createElement("div");
@@ -34,34 +36,52 @@ function createGameboardArea() {
 }
 
 function createQuoteArea() {
+  const userName = document.getElementById("nameInput").value;
   const quoteSection = document.createElement("section");
   quoteSection.id = "quoteSection";
   createQuoteBoxes(
     quoteSection,
     allySrc,
-    "Our formation is set and ready for action captain. We are awaiting your orders."
+    `Our formation is set and ready for action captain ${userName}. We are awaiting your orders.`,
+    "allyParagraph",
+    "allyMessageSection",
+    "allyAvatar"
   );
   createQuoteBoxes(
     quoteSection,
     enemySrc,
-    "You will regret the day you started this. I will end you."
+    `You will regret the day you started this ${userName}. Let the war begin.`,
+    "enemyParagraph",
+    "enemyMessageSection",
+    "enemyAvatar"
   );
   return quoteSection;
 }
 
-function createQuoteBoxes(parent, img, string) {
+function createQuoteBoxes(
+  parent,
+  img,
+  string,
+  paragraphId,
+  messageSectionId,
+  avatarId
+) {
   const messageBox = document.createElement("section");
+  messageBox.id = messageSectionId;
   messageBox.className = "message-container";
   parent.appendChild(messageBox);
 
   const avatar = document.createElement("img");
   avatar.src = img;
   avatar.className = "avatar";
+  avatar.id = avatarId;
   avatar.style.height = "80%";
   messageBox.appendChild(avatar);
 
   const quoteParagraph = document.createElement("p");
-  quoteParagraph.textContent = string;
+  quoteParagraph.id = paragraphId;
+  //When message finishes typing start the game.
+  typeMessage(quoteParagraph, string).then(humanTurn);
   messageBox.appendChild(quoteParagraph);
 }
 
