54bf2a6c3f5649a3d65cf27c619e5d4d33c23649
late-bloomer82 <nateriver72@outlook.com>
2024-08-20 11:50:21 -0400

    removed testing ship positions

--- src/classes/ship.js
+++ src/classes/ship.js
@@ -35,23 +35,8 @@ export const playerCruiser = new Ship(3, 0, "floating", "cruiser");
 export const playerSubmarine = new Ship(3, 0, "floating", "submarine");
 export const playerDestroyer = new Ship(2, 0, "floating", "destroyer");
 
-export const computerCarrier = new Ship(5, 0, "floating", "carrier", {
-  left: 20,
-  top: 20,
-});
-export const computerBattleship = new Ship(4, 0, "floating", "battleship", {
-  left: 20,
-  top: 30,
-});
-export const computerCruiser = new Ship(3, 0, "floating", "cruiser", {
-  left: 20,
-  top: 40,
-});
-export const computerSubmarine = new Ship(3, 0, "floating", "submarine", {
-  left: 20,
-  top: 50,
-});
-export const computerDestroyer = new Ship(2, 0, "floating", "destroyer", {
-  left: 20,
-  top: 60,
-});
+export const computerCarrier = new Ship(5, 0, "floating", "carrier");
+export const computerBattleship = new Ship(4, 0, "floating", "battleship");
+export const computerCruiser = new Ship(3, 0, "floating", "cruiser");
+export const computerSubmarine = new Ship(3, 0, "floating", "submarine");
+export const computerDestroyer = new Ship(2, 0, "floating", "destroyer");
